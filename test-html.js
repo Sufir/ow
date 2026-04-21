9e4d2623421078f0cda050f16a3189f3ddcc4975
Sufir <Sufir@users.noreply.github.com>
2026-04-21 08:48:03 +0000

    feat: Шаблон жетонов фабрик для печати
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

---
+++ test-html.js
@@ -0,0 +1,1 @@
+// Just a sanity check: I don't need to change crop marks.
---
+++ test3.js
@@ -0,0 +1,2 @@
+const { execSync } = require('child_process');
+// I can just test this in Node using a minimal DOM, or just realize it's standard CSS behavior.
